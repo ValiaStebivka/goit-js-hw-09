87df6d305cdd6abbe12312eea4ac14ec1ddea8e9
ValiaStebivka <valia.stebivka2016@gmail.com>
2023-12-08 14:43:43 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let changeColorID;
+function getRandomHexColor() {
+  return #${Math.floor(Math.random() * 16777215).toString(16)};
+}
 
+refs.start.addEventListener('click', onStartChangeColor);
+function onStartChangeColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  changeColorID = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+}
+refs.stop.addEventListener('click', onStopChangeColor);
+function onStopChangeColor() {
+  clearInterval(changeColorID);
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
